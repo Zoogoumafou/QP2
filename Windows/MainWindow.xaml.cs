cd3229cbe269889ad383756421c929677eefc7f9
Zoog <lagacem3@outlook.com>
2022-11-15 15:01:44 -0500

    Remove Adonis UI add MAHAPPS METRO

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using QuickPing2.Utils;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using MahApps.Metro.Controls;
 
 
 namespace QuickPing2.Windows
@@ -11,12 +12,8 @@ namespace QuickPing2.Windows
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-
-       
-
-
         public MainWindow()
         {
              JSONHanlder sites = new JSONHanlder();
