52266ff647baee04ff201ecd4093da871a9b1cc1
Zoog <lagacem3@outlook.com>
2022-11-15 12:30:28 -0500

    I dont know what im doing so fuck off

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,26 +12,10 @@ namespace QuickPing2
 
     public partial class App : Application
     {
-        private const int MINIMUM_SPLASH_TIME = 1500; // Miliseconds  
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            Splash splash = new Splash();
-            splash.Show();
-            // Step 2 - Start a stop watch  
-            Stopwatch timer = new Stopwatch();
-            timer.Start();
-
-            // Step 3 - Load your windows but don't show it yet  
-            base.OnStartup(e);
-            MainWindow main = new MainWindow();
-            timer.Stop();
-
-            int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
-            if (remainingTimeToShowSplash > 0)
-                Thread.Sleep(remainingTimeToShowSplash);
-
-            splash.Close();
+           
 
         }
 
