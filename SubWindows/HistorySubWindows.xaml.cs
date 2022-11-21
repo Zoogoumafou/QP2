276504b2b892b4c7cb2aedcc21e56984e0317d56
Zoog <lagacem3@outlook.com>
2022-11-21 15:15:41 -0500

    Starting work with history

--- SubWindows/HistorySubWindows.xaml.cs
+++ SubWindows/HistorySubWindows.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ControlzEx.Standard;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +27,7 @@ namespace QuickPing2.SubWindows
         {
             InitializeComponent();
         }
+
+
     }
 }
--- Utils/JSONHanlder.cs
+++ Utils/JSONHanlder.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices;
 using Newtonsoft.Json.Linq;
 using QuickPing2.Classs;
 
